da6ac3e816bb5c25af811e324bc61b404cd8c53d
kamal-sethi <kamalsethi018@gmail.com>
2025-10-04 13:10:26 +0530

    completed the categories component

---
+++ client/src/app/components/ProductCard.tsx
@@ -0,0 +1,10 @@
+"use client";
+
+import { ProductType } from "@/types";
+
+const ProductCard = ({ product }: { product: ProductType }) => {
+  return <div className="w-full">ProductCard</div>;
+  // return <div className="bg-green-200">ProductCard</div>;
+};
+
+export default ProductCard;
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,14 @@
+import Image from "next/image"
+import ProductList from "./components/ProductList"
+
 const Homepage = () => {
   return (
-    <div className=''>Homepage</div>
+    <div >
+      <div className='relative aspect-[3/1] mb-12'>
+        <Image src="/featured.png" alt="Featured Product" fill/>
+      </div>
+      <ProductList/>
+    </div>
   )
 }
 
